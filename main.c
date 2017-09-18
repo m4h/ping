7bfeff55ffee48f45e7c587a2000a07e89c410dd
Artyom Klimenko <artyom.klimenko@gmail.com>
2017-09-18 23:02:14 +0300

    refactor: made var names clear, removed dead code

--- main.c
+++ main.c
@@ -10,6 +10,23 @@
 #include <time.h>
 
 /*
+ * why need to use this:
+ *  expose icmp packet options
+ *  cool displays (json)
+ *
+ * structure of program:
+ * main
+ *  read options and set defaults
+ *  loop
+ *    send_packet
+ *    display
+ *      display_type
+ *  display statistics
+ *    display_type
+ *
+*/
+
+/*
   gcc main.c -o p; sudo chown root:root p; sudo chmod ugo+rxs p
 
   - server uptime by tsval
@@ -19,7 +36,7 @@
   FIXME: timeout issue (actually timeout doesn't work as expected)
 */
 
-struct cmd_opts {
+struct options {
   const char    *ip;
   uint16_t      icmp_type;
   uint32_t      icmp_idi;
@@ -42,7 +59,8 @@ struct accounting {
   long  packets;
 };
 
-uint16_t icmp_csum(uint16_t *h, uint32_t l)
+
+uint16_t icmp_checksum(uint16_t *h, uint32_t l)
 {
   unsigned long csum = 0;
   while (l > 1) {
@@ -58,19 +76,26 @@ uint16_t icmp_csum(uint16_t *h, uint32_t l)
 }
 
 
-void do_display_errno(int e)
+void *do_malloc(int len)
 {
-  switch(e) {
-    case 101:
-      printf("errno %d: network is unreachable\n", e);
-      break;
-    default:
-      printf("errno: %d\n", e);
+  void *ptr = malloc(len);
+  if (ptr == NULL) {
+    printf("error: failed to allocate %d bytes. errno: %d\n", len, errno);
+    return ptr;
+  }
+  memset(ptr, 0, len);
+  return ptr;
+}
+
+void do_free(void *ptr)
+{
+  if (ptr != NULL) {
+    free(ptr);
+    ptr = NULL;
   }
 }
 
-
-void do_display_graph(struct accounting *s, struct cmd_opts *o, double rtt)
+void do_display_graph(struct accounting *s, struct options *o, double rtt)
 {
   if (s->packets == 1) {
     printf("--- rtt based graph: '!' is more than %.fms; '-' is less than %.fms; 't' is timeout (%ds) ---\n", o->max_rtt, o->min_rtt, o->timeout);
@@ -100,102 +125,102 @@ void do_display_summary(struct accounting *s)
 }
 
 
-int do_open_socket(struct cmd_opts *o)
+int do_open_socket(struct options *options)
 {
-  int s = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
-  if (s < 0) {
+  int socket_fd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+  if (socket_fd < 0) {
     printf("error: failed to open socket. errno: %d\n", errno);
     return -1;
   }
   // set TTL on IP packet 
-  int rc = setsockopt(s, IPPROTO_IP, IP_TTL, &o->ttl, sizeof(o->ttl));
+  int rc = setsockopt(socket_fd, IPPROTO_IP, IP_TTL, &options->ttl, sizeof(options->ttl));
   if (rc != 0) {
     printf("error: failed to set ttl. errno: %d\n", errno);
     return -1;
   }
-  return s;
+  return socket_fd;
 }
 
 
-// FIXME: pass buffer to hold icmp packet, after copy from internal buffer to arg buffer and release internal buffers
-// the idea - each scope manage his own buffers and there are no pointers travel between functions
-int do_send_icmp(int s, struct cmd_opts *o, void *p)
+double do_timespec_delta(struct timespec s, struct timespec e)
+{
+  /* 
+   * find delta in ms between two struct timespec
+   * @s - start timespec
+   * @e - end timespec (
+   * return - delta in milliseconds
+   */
+  double ms = (e.tv_sec - s.tv_sec) * 1000.0;
+  ms += (e.tv_nsec - s.tv_nsec) / 1000000.0;
+  return ms;
+}
+
+
+int do_send_icmp(int socket_fd, struct options *options, void *packet)
 {
   // create ipv4 header
-  struct in_addr ip_aton;
-  struct sockaddr_in ip_hdr;
-  inet_aton(o->ip, &ip_aton);
-  memset(&ip_hdr, 0, sizeof(ip_hdr));
-  ip_hdr.sin_family = AF_INET;
-  ip_hdr.sin_addr = ip_aton;
+  struct sockaddr_in ip_header;
+  struct in_addr ip_addrr;
+  inet_aton(options->ip, &ip_addrr);
+  memset(&ip_header, 0, sizeof(ip_header));
+  ip_header.sin_family = AF_INET;
+  ip_header.sin_addr = ip_addrr;
 
   // create icmp header
-  // icmp header length
-  int hlen = sizeof(struct icmphdr);
-  // phdr is used as icmp header buffer (sent and recv)
-  struct icmphdr *phdr = malloc(hlen);
-  if (phdr == NULL) {
-    printf("error: failed to allocate %d bytes. errno: %d\n", hlen, errno);
+  int icmp_hdr_len = sizeof(struct icmphdr);
+  // icmp_hdr_ptr is used as icmp header buffer (sent and recv)
+  struct icmphdr *icmp_hdr_ptr = do_malloc(icmp_hdr_len);
+  if (icmp_hdr_ptr == NULL) {
     return -1;
   }
-  memset(phdr, 0, hlen);
-  phdr->type = o->icmp_type;
-  phdr->un.echo.sequence = htons(o->icmp_sequence++);
-  phdr->un.echo.id = htons(o->icmp_idi);
-  phdr->checksum = 0;
+  icmp_hdr_ptr->type = options->icmp_type;
+  icmp_hdr_ptr->un.echo.sequence = htons(options->icmp_sequence++);
+  icmp_hdr_ptr->un.echo.id = htons(options->icmp_idi);
+  icmp_hdr_ptr->checksum = 0;
 
   // create icmp packet (header + payload)
-  // ppckt is used as icmp packet buffer (sent and recv)
-  char *ppckt = malloc(o->icmp_len);
-  if (ppckt == NULL) {
-    printf("error: failed to allocate %d bytes. errno: %d\n", o->icmp_len, errno);
+  // packet_ptr is used as icmp packet buffer (sent and recv)
+  char *packet_ptr = do_malloc(options->icmp_len);
+  if (packet_ptr == NULL) {
     return -1;
   }
-  memset(ppckt, 0, o->icmp_len);
   // copy icmp header to packet buffer
-  memcpy(ppckt, phdr, hlen);
+  memcpy(packet_ptr, icmp_hdr_ptr, icmp_hdr_len);
   // copy icmp payload to packet buffer
-  memcpy(ppckt + hlen, o->icmp_payload, strlen(o->icmp_payload));
-  int plen = hlen + strlen(o->icmp_payload);
-  uint16_t csum = icmp_csum((uint16_t*)ppckt, plen);
-  phdr->checksum = csum;
+  memcpy(packet_ptr + icmp_hdr_len, options->icmp_payload, strlen(options->icmp_payload));
+  int packet_len = icmp_hdr_len + strlen(options->icmp_payload);
+  uint16_t checksum = icmp_checksum((uint16_t*)packet_ptr, packet_len);
+  icmp_hdr_ptr->checksum = checksum;
   // copy icmp header with correct checksum
-  memcpy(ppckt, phdr, hlen);
+  memcpy(packet_ptr, icmp_hdr_ptr, icmp_hdr_len);
   
   // send packet over wire
-  int rc = sendto(s, ppckt, plen, 0, (struct sockaddr*)&ip_hdr, sizeof(ip_hdr));
+  sendto(socket_fd, packet_ptr, packet_len, 0, (struct sockaddr*)&ip_header, sizeof(ip_header));
   // wait for response
-  struct timeval tv = {o->timeout, 0};
-  fd_set rfd;
-  FD_ZERO(&rfd);
-  FD_SET(s, &rfd);
-  rc = select(s + 1, &rfd, NULL, NULL, &tv);
+  struct timeval timeout = {options->timeout, 0};
+  fd_set read_set;
+  FD_ZERO(&read_set);
+  FD_SET(socket_fd, &read_set);
+  int rc = select(socket_fd + 1, &read_set, NULL, NULL, &timeout);
   if (rc == -1) {
     printf("error: failed to read icmp packet. errno:%d\n", errno);
     return -1;
   }
-  // read response from socket and write to ppckt buffer
-  memset(ppckt, 0, o->icmp_len);
-  rc = recvfrom(s, ppckt, o->icmp_len, MSG_DONTWAIT, NULL, NULL);
-  memcpy(p, ppckt, o->icmp_len);
-  /*
-  // since SOCK_RAW is used - ppckt will hold 20 bytes of ipv4 header
-  memset(phdr, 0, hlen);
-  memcpy(phdr, ppckt + 20, hlen);
-  */
-  if (ppckt != NULL) {
-    free(ppckt);
-  }
+  // read response from socket and write to packet_ptr buffer
+  memset(packet_ptr, 0, options->icmp_len);
+  rc = recvfrom(socket_fd, packet_ptr, options->icmp_len, MSG_DONTWAIT, NULL, NULL);
+  memcpy(packet, packet_ptr, options->icmp_len);
+  do_free(icmp_hdr_ptr);
+  do_free(packet_ptr);
   return rc;
 }
 
 
-int do_icmp(struct cmd_opts *opts)
+int do_main_loop(struct options *options)
 {
-  int sockfd = do_open_socket(opts);
+  double rtt;
   // variables used to compute and store rtt (round trip time) value
   struct timespec tstart, tend;
-  double rtt;
   // ttl (up to 256)
   unsigned char ttl;
   // accounting
@@ -204,51 +229,51 @@ int do_icmp(struct cmd_opts *opts)
   stats.tot_ms = 0;
   stats.packets = 0;
 
-  for (; opts->packets != 0; opts->packets--) {
-    // FIXME
-    int hlen = sizeof(struct icmphdr);
-    struct icmphdr *phdr = malloc(hlen);
-
-    char *buf = malloc(sizeof(char) * opts->icmp_len);
-    memset(buf, 0, opts->icmp_len);
-    clock_gettime(CLOCK_REALTIME, &tstart);
-    int rc = do_send_icmp(sockfd, opts, buf);
-    
-    if (rc == 0) {
-      printf("error: icmp connection was reset\n");
+  int socket_fd = do_open_socket(options);
+  for (; options->packets != 0; options->packets--) {
+    int icmp_hdr_len = sizeof(struct icmphdr);
+    struct icmphdr *icmp_hdr_ptr = do_malloc(icmp_hdr_len);
+    if (icmp_hdr_ptr == NULL) {
       return -1;
+    }
+    char *icmp_packet_ptr = do_malloc(sizeof(char) * options->icmp_len);
+    if (icmp_packet_ptr == NULL) {
+      return -1;
+    }
+
+    clock_gettime(CLOCK_REALTIME, &tstart);
+    int rc = do_send_icmp(socket_fd, options, icmp_packet_ptr);
+    clock_gettime(CLOCK_REALTIME, &tend);
+    if (rc == 0) {
+      printf("error: icmp connection was reset. errno:%d\n", errno);
     } else if (rc == -1) {
       // EAGAIN indicate that select(3) hit timeout and recvfrom(3) don't have data to read
-      // FIXME: ugly stuff
       if (errno == EAGAIN) {
-        // reset readfd and skip the packet
-        /*
-        free(phdr);
-        free(ppckt);
-        */
-        // FIXME: packets++ is not correct as after calculation done on these packets. need to introduce chars variable
-        stats.packets++;
-        do_display_graph(&stats, opts, -1);
-        continue;
+        printf("error: icmp connection timeout. errno:%d\n", errno);
       } else {
         printf("error: icmp connection was interrupted. errno:%d\n", errno);
-        return errno;
       }
-    } else if (rc < (int)sizeof(hlen)) {
-      printf("error: got packet shorter than header\n");
-      return errno;
+    } else if (rc < (int)sizeof(icmp_hdr_len)) {
+      printf("error: received icmp packet shorter than icmp header\n");
+    } else {
+      // rtt - round trip time is a time in ms which took for packet to travel (forth and back)
+      rtt = do_timespec_delta(tstart, tend);
+      memset(&ttl, 0, sizeof(ttl));
+      // FIXME: rewrite ugly hack with 9th byte offset - get ttl from ip header (ttl is 9th byte)
+      memcpy(&ttl, icmp_packet_ptr + 8, sizeof(ttl));
+      // reuse of icmp_hdr_ptr to fetch returned sequence
+      memset(icmp_hdr_ptr, 0, icmp_hdr_len);
+      // since SOCK_RAW is used - icmp_packet_ptr will hold 20 bytes of ipv4 header (which we need to strip off)
+      memcpy(icmp_hdr_ptr, icmp_packet_ptr + 20, icmp_hdr_len);
+      if (icmp_hdr_ptr->type == ICMP_ECHOREPLY) {
+        printf("seq=%d; time=%.1fms; ttl=%d\n", ntohs(icmp_hdr_ptr->un.echo.sequence), rtt, ttl);
+      } else {
+        printf("error: received wrong icmp packet type:%d\n", icmp_hdr_ptr->type);
+      }
     }
-    clock_gettime(CLOCK_REALTIME, &tend);
-    // rtt is stored in ms - hence we need to convert tv_sec and tv_nsec to ms
-    rtt = (tend.tv_sec - tstart.tv_sec) * 1000.0;
-    rtt += (tend.tv_nsec - tstart.tv_nsec) / 1000000.0;
-
-    memset(phdr, 0, hlen);
-    memcpy(phdr, buf + 20, hlen);
-    memset(&ttl, 0, sizeof(unsigned char));
-    // FIXME: rewrite ugly hack with 9th byte offset - get ttl from ip header (ttl is 9th byte)
-    memcpy(&ttl, buf + 8, sizeof(unsigned char));
-
+    do_free(icmp_hdr_ptr);
+    do_free(icmp_packet_ptr);
+    
     // account statistic
     stats.packets++;
     stats.tot_ms += rtt;
@@ -258,33 +283,19 @@ int do_icmp(struct cmd_opts *opts)
     if (rtt < stats.min_ms) {
       stats.min_ms = rtt;
     }
-    if (phdr->type == ICMP_ECHOREPLY) {
-      printf("recv: seq=%d; time=%.1fms; ttl=%d\n", ntohs(phdr->un.echo.sequence), rtt, ttl);
-      //do_display_graph(&stats, opts, rtt);
-    } else {
-      printf("recv: icmp packet with wrong type : %d\n", phdr->type);
-    }
 
-    if (phdr != NULL) {
-      free(phdr);
-      phdr = NULL;
-    }
-    if (buf != NULL) {
-      free(buf);
-      buf = NULL;
-    }
     // FIXME: convert to nanosleep
-    sleep(opts->interval);
+    sleep(options->interval);
   }
   do_display_summary(&stats);
-  close(sockfd);
+  close(socket_fd);
   return 0;
 }
 
 
 int main(int argc, char **argv)
 {
-  struct cmd_opts opts;
+  struct options opts;
   opts.ip = argv[1];
   opts.icmp_type = ICMP_ECHO;
   opts.icmp_len = 4096;
@@ -299,7 +310,6 @@ int main(int argc, char **argv)
   opts.max_rtt = 100;
   opts.min_rtt = 1;
 
-  int rc = do_icmp(&opts);
-
+  int rc = do_main_loop(&opts);
   return rc;
 }
